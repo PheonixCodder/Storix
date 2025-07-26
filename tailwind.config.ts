720fdf888d7410541120ee27348ffca2c21f713c
Abdulahad <212123310+PheonixCodder@users.noreply.github.com>
2025-07-26 23:51:43 +0500

    Completed Final Build

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -68,11 +68,13 @@ const config: Config = {
   				'2': 'hsl(var(--chart-2))',
   				'3': 'hsl(var(--chart-3))',
   				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
+  				'5': 'hsl(var(--chart-5))',
   			}
   		},
   		fontFamily: {
-  			poppins: ["var(--font-poppins)"]
+  			poppins: [
+  				'var(--font-poppins)'
+  			]
   		},
   		boxShadow: {
   			'drop-1': '0px 10px 30px 0px rgba(66, 71, 97, 0.1)',
